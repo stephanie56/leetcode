17ac7f6679fbd0ecc1da2a41fc0f5afed4c7d106
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-11-21 22:01:41 -0500

    Added climbstairs challenge

---
+++ climbStairs.js
@@ -0,0 +1,26 @@
+/**
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+**/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+function climbStairs (n) {
+
+  var combination = 0;
+
+  // case 1: climb stairs with 1 step only [1,1,1,1]
+
+
+  // case 2: climb stairs with 2 steps [2,1], [1,2]
+
+
+
+  return steps;
+};
+
+module.exports = climbStairs;
---
+++ test/climbStairs.test.js
@@ -0,0 +1,5 @@
+const climbStairs = require('../climbStairs');
+
+test('first test', () => {
+  expect(climbStairs(3)).toBe(3);
+});
